92d4b61895fdd527e80bb7da39f4549c693d417e
Yeungbin Lee <yeungbinl@gmail.com>
2025-05-11 15:24:31 +0900

    Update app.py

--- DashBoard/app.py
+++ DashBoard/app.py
@@ -6,23 +6,23 @@ from visualization import display_metrics, create_map, create_charts
 st.set_page_config(page_title="의료 현황 대시보드", layout="wide", page_icon="\U0001F3E5")
 st.header('전국 의료 현황 대시보드\U0001F4C8', divider='gray')
 
-# 데이터 로드 및 필터링
+# Data Load
 df = load_data()
 selected_region, selected_city, filtered_df = filter_data(df)
 
-# 지표 선택
+# Category to Visualize
 indicator = st.radio("지표를 선택하세요:", ['의료접근성지표', '전공불균형지표'],
                      help="의료접근성 지표 계산 방법: 인구 10만 명 당 의사수/병원수/병상수/미충족 의료율 정규화 후 25점씩 가중치 부여해 100점 만점 | 전공불균형 지표 계산 방법: 해당 지역의 필수과 비율 - 0.38",
                      label_visibility="visible")
 
-# 지표 표시
+# Legend
 display_metrics(df, filtered_df, selected_region, selected_city)
 
-# 지도 및 차트 생성
+# Map Construction
 m = create_map(filtered_df, selected_region, selected_city, indicator)
 fig1, fig2 = create_charts(filtered_df, selected_region, selected_city)
 
-# 시각화 표시
+# Visualization 
 with st.container():
     st.markdown("---")
     st.markdown("#### 지표 시각화",
